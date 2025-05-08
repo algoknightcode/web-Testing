1456c42907576c531311c7761c1920da21cdbe91
algoknightcode <algoknightcode@gmail.com>
2025-05-09 05:26:11 +0530

    first commit

---
+++ tests/HomePage.spec.js
@@ -0,0 +1,29 @@
+const { test, expect } = require('@playwright/test');
+
+test('home page of Demoblaze', async ({ page }) => {
+  // Visit Demoblaze
+  await page.goto('https://portfolio-eight-ashy-54.vercel.app/');
+
+  // Wait and get the title
+  const pageTitle = await page.title();
+  console.log("Page title is:", pageTitle);
+
+   // Validate main heading text
+   await expect(page.locator('text=Crafting Scalable & Engaging Web Experiences')).toBeVisible();
+
+    // Check Resume link/button
+  await expect(page.locator('text=Resume')).toBeVisible();
+
+
+  // Validate the page title (check actual casing)
+  await expect(page).toHaveTitle('Create Next App');  // It's in uppercase
+
+  // Get and log the URL
+  const pageUrl = page.url();
+  console.log('Page URL is:', pageUrl);
+
+  // Validate the URL
+  await expect(page).toHaveURL("https://portfolio-eight-ashy-54.vercel.app/");
+
+  await page.close();
+});
